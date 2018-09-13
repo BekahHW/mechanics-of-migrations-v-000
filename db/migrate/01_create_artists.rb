b52bb6a514278f7ba92ff8980347d800efe1084f
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-09-13 17:50:31 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -27,5 +27,10 @@ ActiveRecord::Base.connection.execute(sql)
 
 def change
   create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
   end
 end
