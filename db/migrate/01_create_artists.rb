737ad195805b9bea0eb14699e2867f3ff7a45735
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-09-13 17:44:00 +0000

    automatically backed up by learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -7,4 +7,9 @@ class CreateArtists < ActiveRecord::Migration
   
   def change
   end
+  
+  connection = ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/artists.sqlite"
+)
 end
